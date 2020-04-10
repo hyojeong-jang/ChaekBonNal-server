407a922f9b817f9317d4061221c36378d9a04f80
Hyojeong Jang <hyojeong@Hyojeongui-MacBookPro.local>
2020-04-10 15:50:45 +0900

    complete CRUD function in comment

--- routes/bookReports.js
+++ routes/bookReports.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
+const Comment = require('../models/comment');
 const BookReport = require('../models/bookReport');
 const { authorization } = require('../middlewares/authorization');
 
@@ -9,7 +10,12 @@ router.get('/:report_id', async (req, res) => {
 
     try {
         const bookReport = await BookReport.findById(report_id);
-        res.status(200).json({ success: true, bookReport });
+        const comments = await Comment.find({ '_id': { $in: bookReport.comments } }).populate('author');
+
+        res.status(200).json({ 
+            bookReport,
+            comments
+        });
     } catch (error) {
         res.status(400).json({ success: false })
     }
@@ -19,7 +25,7 @@ router.put('/:report_id/users/:user_id/bookmark', authorization, async (req, res
     const { email } = res.locals;
     const { report_id } = req.params;
     const { isBookmarked } = req.body;
-    console.log(isBookmarked)
+
     try {
         if (isBookmarked) {
             await User.findOneAndUpdate(
@@ -41,5 +47,51 @@ router.put('/:report_id/users/:user_id/bookmark', authorization, async (req, res
     }
 
 })
+
+router.post('/:report_id/users/:user_id/comment', authorization, async (req, res) => {
+    const { report_id } = req.params;
+    const { email } = res.locals;
+    const { comment } = req.body.data;
+
+    const author = await User.findOne({ email }, '_id');
+
+    const comments = await Comment.create({
+        text: comment,
+        author: author._id,
+        date: Date.now().toString()
+    });
+
+    const bookReport = await BookReport.findByIdAndUpdate(
+        report_id,
+        { $push: { comments } },
+        { new: true }
+    );
+
+    const bookReportComments = await Comment.find(
+        { '_id': { $in: bookReport.comments } }
+    ).populate('author');
+
+   res.status(200).json({ comments: bookReportComments });
+});
+
+router.delete('/:report_id/users/:user_id/comment', authorization, async (req, res) => {
+    const { report_id } = req.params;
+    const { commentId } = req.body;
+
+    await Comment.findByIdAndDelete(commentId);
+
+    const bookReport = await BookReport.findByIdAndUpdate(
+        report_id,
+        { $pull: { comments: commentId } },
+        { new: true }
+    );
+
+    const comments = await Comment.find(
+        { '_id': { $in: bookReport.comments } }
+    ).populate('author');
+
+
+    res.status(200).json({ comments })
+});
 
 module.exports = router;
