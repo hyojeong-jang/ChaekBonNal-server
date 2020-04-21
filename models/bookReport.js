8c04107b698169dda3fd5ce3f86d47f045d6cfd4
Hyojeong Jang <hyojeong@Hyojeongui-MacBookPro.local>
2020-04-21 15:35:15 +0900

    modified model

--- models/bookReport.js
+++ models/bookReport.js
@@ -2,43 +2,43 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const bookReportSchema = mongoose.Schema({
-    image_url: String,
-    text: String,
-    author: {
-        type: Schema.ObjectId,
-        ref: 'User'
-    },
-    book_info: {
-        title: {
-            type: String,
-            required: true
-        },
-        author: {
-            type: String,
-            required: true
-        },
-        category: {
-            type: String,
-            required: true
-        },
-        publisher: {
-            type: String,
-            required: true
-        },
-        link: String,
-        image: String
-    },
+  image_url: String,
+  text: String,
+  author: {
+    type: Schema.ObjectId,
+    ref: 'User'
+  },
+  book_info: {
     title: {
-        type: String,
-        required: true
+      type: String,
+      required: true
     },
-    quote: String,
-    comments: [
-        {
-            type: Schema.ObjectId,
-            ref: 'Comment'
-        }
-    ]
+    author: {
+      type: String,
+      required: true
+    },
+    category: {
+      type: String,
+      required: true
+    },
+    publisher: {
+      type: String,
+      required: true
+    },
+    link: String,
+    image: String
+  },
+  title: {
+    type: String,
+    required: true
+  },
+  quote: String,
+  comments: [
+    {
+      type: Schema.ObjectId,
+      ref: 'Comment'
+    }
+  ]
 });
 
 module.exports = mongoose.model('BookReport', bookReportSchema);
