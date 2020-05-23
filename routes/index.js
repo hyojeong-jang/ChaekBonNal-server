5f19c82649c37e169eaa4adc6391ab80cda02bc0
Hyojeong Jang <hyojeong@Hyojeongui-MacBookPro.local>
2020-05-23 13:34:10 +0900

    set default path

--- routes/index.js
+++ routes/index.js
@@ -4,31 +4,35 @@ const BookReport = require('../models/bookReport');
 const User = require('../models/user');
 const jwt = require('jsonwebtoken');
 
+router.get('/', (req, res) => {
+  res.render('index')
+})
+
 router.post('/login', async (req, res) => {
-    const { imageUrl, name, email } = req.body.response.profileObj;
+  const { imageUrl, name, email } = req.body.response.profileObj;
 
-    try {
-        const userData = await User.findOneAndUpdate(
-            { email },
-            { imageUrl, name, email },
-            { upsert: true, new: true }
-        );
+  try {
+    const userData = await User.findOneAndUpdate(
+      { email },
+      { imageUrl, name, email },
+      { upsert: true, new: true }
+    );
 
-        const token = jwt.sign(
-            { email },
-            process.env.SECRET_KEY,
-            { expiresIn: '7d' }
-        );
+    const token = jwt.sign(
+      { email },
+      process.env.SECRET_KEY,
+      { expiresIn: '7d' }
+    );
 
-        res.status(200).json({ token, userData });
-    } catch (error) {
-        console.log(error)
-    }
+    res.status(200).json({ token, userData });
+  } catch (error) {
+    console.log(error)
+  }
 });
 
 router.get('/non-member/book-reports', async (req, res) => {
-    const bookReports = await BookReport.find().populate('author');
-    res.status(200).json({ bookReports });
+  const bookReports = await BookReport.find().populate('author');
+  res.status(200).json({ bookReports });
 });
 
 module.exports = router;
