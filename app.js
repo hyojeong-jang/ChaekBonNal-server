5d1de58fe3937d23915bc6ebfa2736f8e6137caa
Hyojeong Jang <hyojeong@Hyojeongui-MacBookPro.local>
2020-05-23 13:06:45 +0900

    added view engine

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ const bookReportsRouter = require('./routes/bookReports');
 
 const app = express();
 
+app.set('view engine', 'ejs');
 app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
